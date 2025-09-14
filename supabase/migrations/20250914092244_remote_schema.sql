1692f3e9fa71a578a2b921d563644af248531242
Puranpal Singh <singhpuranpal12@gmail.com>
2025-09-14 14:54:51 +0530

    feat: pull initial database schema and RLS policies

---
+++ supabase/migrations/20250914092244_remote_schema.sql
@@ -0,0 +1,418 @@
+
+\restrict cU7CUszad0pfhlRo5F6d5r9teTlV8gocG9ueIaTV2JSWdUsVedK4PPuLk8gdmsf
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+COMMENT ON SCHEMA "public" IS 'standard public schema';
+
+
+
+CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
+
+
+
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+
+CREATE TABLE IF NOT EXISTS "public"."classroom" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "name" "text" NOT NULL,
+    "school_id" "uuid",
+    "teacher_id" "uuid"
+);
+
+
+ALTER TABLE "public"."classroom" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."profile" (
+    "id" "uuid" NOT NULL,
+    "role" "text" NOT NULL,
+    "school_id" "uuid",
+    "full_name" "text" NOT NULL,
+    CONSTRAINT "profile_role_check" CHECK (("role" = ANY (ARRAY['head_teacher'::"text", 'teacher'::"text", 'student'::"text"])))
+);
+
+
+ALTER TABLE "public"."profile" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."progress" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "student_id" "uuid",
+    "classroom_id" "uuid",
+    "score" integer,
+    "subject" "text" NOT NULL,
+    CONSTRAINT "progress_score_check" CHECK ((("score" >= 0) AND ("score" <= 100)))
+);
+
+
+ALTER TABLE "public"."progress" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."schools" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "name" "text" NOT NULL
+);
+
+
+ALTER TABLE "public"."schools" OWNER TO "postgres";
+
+
+ALTER TABLE ONLY "public"."classroom"
+    ADD CONSTRAINT "classroom_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."profile"
+    ADD CONSTRAINT "profile_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."progress"
+    ADD CONSTRAINT "progress_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."schools"
+    ADD CONSTRAINT "schools_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."classroom"
+    ADD CONSTRAINT "classroom_school_id_fkey" FOREIGN KEY ("school_id") REFERENCES "public"."schools"("id") ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."classroom"
+    ADD CONSTRAINT "classroom_teacher_id_fkey" FOREIGN KEY ("teacher_id") REFERENCES "public"."profile"("id") ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."profile"
+    ADD CONSTRAINT "profile_id_fkey" FOREIGN KEY ("id") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."profile"
+    ADD CONSTRAINT "profile_school_id_fkey" FOREIGN KEY ("school_id") REFERENCES "public"."schools"("id") ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."progress"
+    ADD CONSTRAINT "progress_classroom_id_fkey" FOREIGN KEY ("classroom_id") REFERENCES "public"."classroom"("id") ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."progress"
+    ADD CONSTRAINT "progress_student_id_fkey" FOREIGN KEY ("student_id") REFERENCES "public"."profile"("id") ON DELETE CASCADE;
+
+
+
+
+
+ALTER PUBLICATION "supabase_realtime" OWNER TO "postgres";
+
+
+GRANT USAGE ON SCHEMA "public" TO "postgres";
+GRANT USAGE ON SCHEMA "public" TO "anon";
+GRANT USAGE ON SCHEMA "public" TO "authenticated";
+GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+GRANT ALL ON TABLE "public"."classroom" TO "anon";
+GRANT ALL ON TABLE "public"."classroom" TO "authenticated";
+GRANT ALL ON TABLE "public"."classroom" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."profile" TO "anon";
+GRANT ALL ON TABLE "public"."profile" TO "authenticated";
+GRANT ALL ON TABLE "public"."profile" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."progress" TO "anon";
+GRANT ALL ON TABLE "public"."progress" TO "authenticated";
+GRANT ALL ON TABLE "public"."progress" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."schools" TO "anon";
+GRANT ALL ON TABLE "public"."schools" TO "authenticated";
+GRANT ALL ON TABLE "public"."schools" TO "service_role";
+
+
+
+
+
+
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES TO "service_role";
+
+
+
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS TO "service_role";
+
+
+
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES TO "service_role";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+\unrestrict cU7CUszad0pfhlRo5F6d5r9teTlV8gocG9ueIaTV2JSWdUsVedK4PPuLk8gdmsf
+
+RESET ALL;
